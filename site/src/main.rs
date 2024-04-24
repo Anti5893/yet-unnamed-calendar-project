d220815f6f50379df308bfeedd2932f1227ef319
PokeJofeJr4th <PokeJofeJr4th@gmail.com>
2024-04-23 22:30:15 -0400

    convince clippy to complain about implicit returns

--- site/src/main.rs
+++ site/src/main.rs
@@ -1,7 +1,7 @@
 // the compiler will yell at you but it knows what is best for you
-#![warn(clippy::pedantic, clippy::nursery)]
+#![warn(clippy::pedantic, clippy::nursery, clippy::implicit_return)]
 // rocket generated code doesn't properly silence this warning
-#![allow(clippy::no_effect_underscore_binding)]
+#![allow(clippy::no_effect_underscore_binding, clippy::needless_return)]
 
 #[macro_use]
 extern crate rocket;
@@ -9,20 +9,22 @@ use rocket::fs::{relative, FileServer};
 
 fn get_component(file_name: &str) -> String {
     let path: String = format!("components/{file_name}.html");
-    std::fs::read_to_string(path).expect("File doesn't exist")
+    return std::fs::read_to_string(path).expect("File doesn't exist");
 }
 
 #[post("/clicked")]
 fn clicked() -> String {
-    get_component("buttonClicked").replace("{{message}}", "Garfield I dont like mondays")
+    return get_component("buttonClicked").replace("{{message}}", "Garfield I dont like mondays");
 }
 
 #[get("/clicked_twice")]
 fn clicked_twice() -> String {
-    get_component("extra")
+    return get_component("extra");
 }
 
 #[launch]
+// rocket needs the implicit return
+#[allow(clippy::implicit_return)]
 fn rocket() -> _ {
     rocket::build()
         .mount("/", FileServer::from(relative!("/frontend")))
