3d0ce0a41c030b59ae2353aa583e6c1ace6fa745
PokeJofeJr4th <PokeJofeJr4th@gmail.com>
2024-04-23 22:12:08 -0400

    Look at me. I am the captain now

--- README.md
+++ README.md
@@ -1,13 +1,15 @@
 # yet-unnamed-calendar-project
 
 ## Stack:
+
 HTMX\
 Rocket\
 TailwindCSS
 
 ## Build process: (cd ./site)
-Build tailwind: npx tailwindcss -i ./static/css/main.css -o ./static/css/tailwind.css\
-Build and Run Rocket: cargo build && cargo run\
+
+Build tailwind: npx tailwindcss -i ./frontend/css/main.css -o ./frontend/css/tailwind.css\
+Build and Run Rocket: cargo run\
 Serves to localhost:8000/index\
 \
 Router explanation in main.rs
--- site/src/main.rs
+++ site/src/main.rs
@@ -1,20 +1,27 @@
+// the compiler will yell at you but it knows what is best for you
+#![warn(clippy::pedantic, clippy::nursery)]
+// rocket generated code doesn't properly silence this warning
+#![allow(clippy::no_effect_underscore_binding)]
+
 #[macro_use]
 extern crate rocket;
 use rocket::fs::{relative, FileServer};
 
 fn get_component(file_name: &str) -> String {
     let path: String = format!("components/{file_name}.html");
-    return std::fs::read_to_string(path).expect("File doesn't exist");
+    std::fs::read_to_string(path).expect("File doesn't exist")
 }
 
 #[post("/clicked")]
 fn clicked() -> String {
-    return get_component("buttonClicked").replace("{{message}}", "Garfield I dont like mondays");
+    get_component("buttonClicked").replace("{{message}}", "Garfield I dont like mondays")
 }
+
 #[get("/clicked_twice")]
 fn clicked_twice() -> String {
-    return get_component("extra");
+    get_component("extra")
 }
+
 #[launch]
 fn rocket() -> _ {
     rocket::build()
